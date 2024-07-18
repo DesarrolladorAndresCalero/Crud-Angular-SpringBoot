155b46b7b946c7d7dae3071e439259b6d8bdf8ad
DesarrolladorAndresCalero <desarrollador.andrescalero@gmail.com>
2024-07-17 19:28:53 -0500

    se agrega la carpeta de services

--- personaFrontend/src/app/app.component.ts
+++ personaFrontend/src/app/app.component.ts
@@ -1,13 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EstadosService } from './services/estados/estados.service';
+import { PaisesService } from './services/paises/paises.service';
+import { PersonaService } from './services/persona/persona.service';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HttpClientModule, ReactiveFormsModule],
+  providers: [    EstadosService,    PaisesService,  PersonaService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'personaFrontend';
+
+export class AppComponent implements OnInit {
+
+  personaForm: FormGroup;
+  paises: any;
+  estado: any;
+  persona: any;
+
+  constructor(
+    private fb: FormBuilder,
+    public estadosService: EstadosService,
+    public paisesService: PaisesService,
+    public personaService: PersonaService
+  ) {}
+
+  ngOnInit(): void {
+    this.personaForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      edad: ['', Validators.required],
+      pais: ['', Validators.required],
+      estado: ['', Validators.required]
+    });
+
+
+   this.paisesService.getAllPaises().subscribe(resp => {
+      this.paises = resp;
+    },
+      error => (console.error(error))
+    )
+
+    this.personaForm.get('pais')?.valueChanges.subscribe(value=>{
+      this.estadosService.getEstadoByPais(value.id).subscribe(resp => {
+        this.estado = resp;
+      },
+        error => (console.error(error))
+      )
+    })
+  }
+
+  guardar() {
+    this.personaService.savePersona(this.personaForm.value).subscribe(resp => {
+      this.estado = resp;
+      console.log(resp)
+    },
+      error => (console.error(error))
+    )
+  }
 }
+
